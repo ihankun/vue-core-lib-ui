7cf603f7f866cb699ffe55e395bc43daf063c832
hankun <ihankun@outlook.com>
2022-05-08 11:30:11 +0800

    初始化结构

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
-import SvgIcon from '@/packages/svg-icon/svg-icon.vue'
+/** http */
+import http from '@/packages/http/http'
+/** layout */
 import DashboardFooter from '@/packages/layout/components/DashboardFooter.vue'
 import DashboardHeader from '@/packages/layout/components/DashboardHeader.vue'
 import DashboardSettingsDrawer from '@/packages/layout/components/DashboardSettingsDrawer.vue'
 import DashboardSidebar from '@/packages/layout/components/DashboardSidebar.vue'
+/** svg-icon */
+import SvgIcon from '@/packages/svg-icon/svg-icon.vue'
 
 const components = [
   SvgIcon,
   DashboardFooter,
   DashboardHeader,
   DashboardSettingsDrawer,
-  DashboardSidebar
+  DashboardSidebar,
+  http
 ]
 
 const install = (Vue: any) => {
@@ -26,6 +31,7 @@ if (typeof window !== 'undefined' && (window as any).Vue) {
 }
 
 export default {
+  version: '0.0.1',
   install,
   ...components
 }
